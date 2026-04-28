f77972e52dafb70af9989d755a92dea38f4c8e65
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-28 21:37:31 +0100

    feat: refactor profiles command structure and add export functionality

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ import { login,
         exportProfiles } from './commands/index.js'
 
 
-// Define the "insighta login" command
+        
 program
 .command('login')
-    .description('Login with GitHub')
-    .action(login) // function called login
-
+.description('Login with GitHub')
+.action(login) // function called login
+        
 
 // insighta logout
 program
@@ -29,89 +29,102 @@ program
     .command('whoami')
     .description('Get User Profile data')
     .action(whoami)
+    
+
+// Define the "insighta login" command
+const profilesCommand = program
+    .command('profiles')
+    .description('Manage profiles')
+
 
 // insighta profiles list command
-// program
-//     .command('profiles list')
-//     .description('send GET request to profiles endpoint')
+profilesCommand
+    .command('list')
+    .description('send GET request to profiles endpoint')
 
-//     .addOption(new Option('--gender <gender>', 'get by gender')
-//     .choices(['male', 'female']))
+    .addOption(new Option('--gender <gender>', 'get by gender')
+    .choices(['male', 'female']))
 
-//     .addOption(new Option('--country <country_id>', 'get by country_id')
-//     .argParser(validateCountryId))
+    .addOption(new Option('--country <country_id>', 'get by country_id')
+    .argParser(validateCountryId))
 
-//     .addOption(new Option('--age-group <age_group>', 'get by age group')
-//     .choices(['child', 'teenager', 'adult', 'senior']))
+    .addOption(new Option('--age-group <age_group>', 'get by age group')
+    .choices(['child', 'teenager', 'adult', 'senior']))
 
-//     .option('--min-age <min_age>', 'get by min age')
-//     .option('--max-age <max_age>', 'get by max age')
-//     .option('--min-gender-probability <min_gender_probability>', 'get by min gender probability')
-//     .option('--min-country-probability <min_country_probability>', 'get by min country probability')
+    .option('--min-age <min_age>', 'get by min age')
+    .option('--max-age <max_age>', 'get by max age')
+    .option('--min-gender-probability <min_gender_probability>', 'get by min gender probability')
+    .option('--min-country-probability <min_country_probability>', 'get by min country probability')
 
-//     .addOption(new Option('--sort-by <sort_by>', 'sort profiles by')
-//     .choices(['age', 'created_at', 'gender_probability']))
+    .addOption(new Option('--sort-by <sort_by>', 'sort profiles by')
+    .choices(['age', 'created_at', 'gender_probability']))
 
-//     .addOption(new Option('--order <order>', 'order profiles by')
-//     .default('asc', 'ascending').choices(['desc', 'asc']))
+    .addOption(new Option('--order <order>', 'order profiles by')
+    .default('asc', 'ascending').choices(['desc', 'asc']))
 
-//     .addOption(new Option('--page <number>', 'page number')
-//     .default(1, 'page one'))
+    .addOption(new Option('--page <number>', 'page number')
+    .default(1, 'page one'))
 
-//     .addOption(new Option('--limit <limit>', 'limit values by')
-//     .default(10, 'ten values')
-//     .argParser(validateLimit))
+    .addOption(new Option('--limit <limit>', 'limit values by')
+    .default(10, 'ten values')
+    .argParser(validateLimit))
 
-//     .action(getProfiles)
-
-
-// // insighta profiles get command
-// program
-//     .command('profiles get')
-//     .argument('<id>', 'id to get by')
-//     .action(getProfilesById)
+    .action(getProfiles)
 
 
-// // insighta profiles search command
-// program
-//     .command('profiles search')
-//     .argument('<query-string>', 'string to query by')
-//     .action(searchProfiles)
+// insighta profiles get command
+profilesCommand
+    .command('get')
+    .argument('<id>', 'id to get by')
+    .action(getProfilesById)
+
+
+// insighta profiles search command
+profilesCommand
+    .command('search')
+    .argument('<query-string>', 'string to query by')
+    .action(searchProfiles)
     
-// // insighta profiles create command
-// program
-//     .command('profiles create')
-//     .option('--name', 'name to process and store')
-//     .argument('<name>', 'name in string')
-//     .action(createProfiles)
+// insighta profiles create command
+profilesCommand
+    .command('create')
+    .option('--name', 'name to process and store')
+    .argument('<name>', 'name in string')
+    .action(createProfiles)
 
-// // insighta profiles export command
-// program
-//     .command('profiles export')
-//     .option('--export', 'specify export format')
-//     .option('--gender <gender>', 'get by gender')
-//     .addOption(new Option('--country <country_id>', 'get by country_id')
-//     .argParser(validateCountryId))
-//     .option('--age-group <age_group>', 'get by age group')
-//     .option('--min-age <min_age>', 'get by min age')
-//     .option('--max-age <max_age>', 'get by max age')
-//     .option('--min-gender-probability <min_gender_probability>', 'get by min gender probability')
-//     .option('--min-country-probability <min_country_probability>', 'get by min country probability')
+// insighta profiles export command
+profilesCommand
+    .command('export')
+    .description('export profiles data in csv format')
+    .option('--export [format]', 'specify export format')
+    .addOption(new Option('--gender <gender>', 'get by gender')
+    .choices(['male', 'female']))
 
-//     .addOption(new Option('--sort-by <sort_by>', 'sort profiles by')
-//     .choices(['age', 'created_at', 'gender_probability']))
+    .addOption(new Option('--country <country_id>', 'get by country_id')
+    .argParser(validateCountryId))
 
-//     .addOption(new Option('--order <order>', 'order profiles by')
-//     .default('asc', 'ascending').choices(['desc', 'asc']))
+    .addOption(new Option('--age-group <age_group>', 'get by age group')
+    .choices(['child', 'teenager', 'adult', 'senior']))
 
-//     .addOption(new Option('--page <number>', 'page number')
-//     .default(1, 'page one'))
+    .option('--min-age <min_age>', 'get by min age')
+    .option('--max-age <max_age>', 'get by max age')
+    .option('--min-gender-probability <min_gender_probability>', 'get by min gender probability')
+    .option('--min-country-probability <min_country_probability>', 'get by min country probability')
 
-//     .addOption(new Option('--limit <limit>', 'limit values by')
-//     .default(10, 'ten values')
-//     .argParser(validateLimit))
+    .addOption(new Option('--sort-by <sort_by>', 'sort profiles by')
+    .choices(['age', 'created_at', 'gender_probability']))
+
+    .addOption(new Option('--order <order>', 'order profiles by')
+    .default('asc', 'ascending').choices(['desc', 'asc']))
+
+    .addOption(new Option('--page <number>', 'page number')
+    .default(1, 'page one'))
+
+    .addOption(new Option('--limit <limit>', 'limit values by')
+    .default(10, 'ten values')
+    .argParser(validateLimit))
     
-//     .action(exportProfiles)
+    .action(exportProfiles)
 
 
 // Process the CLI commands
