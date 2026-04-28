85c49a21b42aa19d7d508e1f076ee778f196dd70
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-28 03:26:02 +0100

    feat: add CLI commands for profiles management and user authentication for connection to required API endpoint, create files for each command

--- index.js
+++ index.js
@@ -12,29 +12,91 @@ program
 
 // insighta logout
 program
-    .command('insighta logout')
+    .command('logout')
     .description('Logout from Insighta Labs Account')
-    .action( () => {
-        // logout - send a get req to logout endpoint
-            
-    })
+    .action(logout)
 
 // insighta whoami
 program
-    .command('insighta whoami')
+    .command('whoami')
     .description('Get User Profile data')
-    .action( () => {
-        // whoami - send a get req to user endpoint with the user id to get info
-            
-    })
+    .action(whoami)
 
+// insighta profiles list command
 program
-    .command('insighta whoami')
-    .description('Get User Profile data')
-    .action( () => {
-        // whoami - send a get req to user endpoint with the user id to get info
-            
-    })
+    .command('profiles list')
+    .description('send GET request to profiles endpoint')
+    .option('--gender <gender>', 'get by gender')
+    .option('--country <country_id>', 'get by country_id')
+    .option('--age-group <age_group>', 'get by age group')
+    .option('--min-age <min_age>', 'get by min age')
+    .option('--max-age <max_age>', 'get by max age')
+    .option('--min-gender-probability <min_gender_probability>', 'get by min gender probability')
+    .option('--min-country-probability <min_country_probability>', 'get by min country probability')
+
+    .addOption(new Option('--sort-by <sort_by>', 'sort profiles by')
+    .choices(['age', 'created_at', 'gender_probability']))
+
+    .addOption(new Option('--order <order>', 'order profiles by')
+    .default(asc, 'ascending')).choices(['desc', 'asc'])
+
+    .addOption(new Option('--page <number>', 'page number')
+    .default(1, 'page one'))
+
+    .addOption(new Option('--limit <limit>', 'limit values by')
+    .default(10, 'ten values'))
+
+    .action(getProfiles)
+
+
+// insighta profiles get command
+program
+    .command('profiles get')
+    .argument('<id>', 'id to get by')
+    .action(getProfilesById)
+
+
+// insighta profiles search command
+program
+    .command('profiles search')
+    .argument('<query-string>', 'string to query by')
+    .action(searchProfiles)
+    
+// insighta profiles create command
+program
+    .command('profiles create')
+    .option('--name', 'name to process and store')
+    .argument('<name>', 'name in string')
+    .action(createProfiles)
+
+// insighta profiles export command
+program
+    .command('profiles export')
+    .option('--export', 'specify export format')
+    .option('--gender <gender>', 'get by gender')
+    .option('--country <country_id>', 'get by country_id')
+    .option('--age-group <age_group>', 'get by age group')
+    .option('--min-age <min_age>', 'get by min age')
+    .option('--max-age <max_age>', 'get by max age')
+    .option('--min-gender-probability <min_gender_probability>', 'get by min gender probability')
+    .option('--min-country-probability <min_country_probability>', 'get by min country probability')
+
+    .addOption(new Option('--sort-by <sort_by>', 'sort profiles by')
+    .choices(['age', 'created_at', 'gender_probability']))
+
+    .addOption(new Option('--order <order>', 'order profiles by')
+    .default(asc, 'ascending')).choices(['desc', 'asc'])
+
+    .addOption(new Option('--page <number>', 'page number')
+    .default(1, 'page one'))
+
+    .addOption(new Option('--limit <limit>', 'limit values by')
+    .default(10, 'ten values'))
+    
+    .action(exportProfiles)
+
 
 // Process the CLI commands
-program.parse(process.argv);
+program.parse(process.argv)
+
+export const options = program.opts()
