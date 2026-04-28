29f011783d5cf57be893639fbf775389a5571645
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-29 00:44:50 +0100

    feat: implement getProfiles function to fetch profiles with query parameters

--- commands/getProfiles.js
+++ commands/getProfiles.js
@@ -1,3 +1,29 @@
-export function getProfiles() {
-    return
+import { fetchResponse, readCredentials } from "../utilFunctions.js"
+
+export async function getProfiles(options) {
+	// get the queries
+	const { gender, country, ageGroup, minAge,
+			maxAge, minGenderProbability, minCountryProbability, 
+			sortBy, order, page, limit } = options
+
+	let paramsObj = {}
+
+	if (gender) paramsObj.gender = gender
+	if (country) paramsObj.country_id = country
+	if (ageGroup) paramsObj.age_group = ageGroup
+	if (minAge) paramsObj.min_age = minAge
+	if (maxAge) paramsObj.max_age = maxAge
+	if (minGenderProbability) paramsObj.min_gender_probability = minGenderProbability
+	if (minCountryProbability) paramsObj.min_country_probability = minCountryProbability
+	if (sortBy) paramsObj.sort_by = sortBy
+	if (order) paramsObj.order = order
+	if (page) paramsObj.page = page
+	if (limit) paramsObj.limit = limit
+
+	const params = new URLSearchParams(paramsObj)
+
+	const response = await fetchResponse('GET', `/api/profiles?${params}`)
+
+	if (response) console.log(response)
+	return
 }
