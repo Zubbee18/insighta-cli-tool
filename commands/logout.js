0595cb3b8ac580c7955c4a100c30faa4ab4f20fa
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-29 20:14:50 +0100

    chore(logout.js): remove logger function, format function to improve readability

--- commands/logout.js
+++ commands/logout.js
@@ -1,22 +1,26 @@
-import { fetchResponse, readCredentials } from "../utilFunctions.js"
-import { logger } from "../logger.js"
-import { createSpinner } from "../spinner.js"
+import { fetchResponse, readCredentials } from "../utilFunctions.js";
+import { createSpinner } from "../spinner.js";
 
 export async function logout() {
-    const spinner = createSpinner('Logging out...')
-    spinner.start()
-    
-    const credentials = await readCredentials()
-    const response = await fetchResponse('POST', '/auth/logout', {refresh_token: credentials.refresh_token}, false)
-    
-    if (!response) {
-        spinner.fail('Logout failed - authentication error')
-        process.exit(1)
-    }
-    
-    if (response) {
-        spinner.succeed(response.message)
-    } else {
-        spinner.fail('Logout failed')
-    }
+  const spinner = createSpinner("Logging out...");
+  spinner.start();
+
+  const credentials = await readCredentials();
+  const response = await fetchResponse(
+    "POST",
+    "/auth/logout",
+    { refresh_token: credentials.refresh_token },
+    false,
+  );
+
+  if (!response) {
+    spinner.fail("Logout failed - authentication error");
+    process.exit(1);
+  }
+
+  if (response) {
+    spinner.succeed(response.message);
+  } else {
+    spinner.fail("Logout failed");
+  }
 }
