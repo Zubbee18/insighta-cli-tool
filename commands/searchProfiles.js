84df276840c11b87c0dfc171aaadedc8358820e7
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-04-29 01:17:15 +0100

    feat: implement searchProfiles function to fetch profiles based on query string

--- commands/searchProfiles.js
+++ commands/searchProfiles.js
@@ -1,3 +1,9 @@
-export function searchProfiles() {
+import { fetchResponse } from "../utilFunctions.js"
+
+export async function searchProfiles(queryString) {
+    const params = new URLSearchParams({q: queryString})
+    const response = await fetchResponse('GET', `/api/profiles/search?${params}`)
+
+    if (response) console.log(response)
     return
 }
